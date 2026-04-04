8874267e69cb3a2ca0ec73035b22b3eeef1429cb
Korrapati Chandana <chandu27122004@gmail.com>
2026-04-04 18:46:16 +0530

    Time: 0 ms (100%), Space: 44.9 MB (19.53%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -4,13 +4,13 @@ class Solution {
         int right=nums.length-1;
 
         while(left<=right) {
-            int mid = (left+right)/1;
+            int mid=(left+right)/2;
 
             if(nums[mid]==target)
             return mid;
             else if(nums[mid] < target)
-            left = mid +1;
-            else 
+            left=mid+1;
+            else
             right=mid-1;
         }
         return left;
