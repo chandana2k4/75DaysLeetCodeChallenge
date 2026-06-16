8a3418011cf536a971ea835c75a444e491c3b72c
Korrapati Chandana <chandu27122004@gmail.com>
2026-06-16 19:31:31 +0530

    Time: 0 ms (100%), Space: 47.3 MB (47.4%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        for(int i=0;i<nums.length;i++) {
+            sum+=nums[i];
+        }
+        int total=n*(n+1)/2;
+        int misnum=total-sum;
+        
+    
+    return misnum;
+}
+}
